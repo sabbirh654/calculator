2a291d45016357f83ff2146f88d5b238e950118a
Sabbir Hasan <sabbirh1000@gmail.com>
2024-05-11 00:05:18 +0600

    Add calculator basic functionality

--- calculator.js
+++ calculator.js
@@ -1,47 +1,39 @@
-function add(a, b)
-{
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a, b)
-{
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a, b)
-{
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a, b)
-{
+function divide(a, b) {
     return a / b;
 }
 
-function modulus(a, b)
-{
+function modulus(a, b) {
     return a % b;
 }
 
-function operate(a, b, operator)
-{
+function operate(a, b, operator) {
     let result = 0;
-
-    switch(operator)
-    {
-        case '+' :
+    switch (operator) {
+        case '+':
             result = add(a, b);
             break;
-        case '-' :
+        case '-':
             result = subtract(a, b);
             break;
-        case '*' :
+        case '*':
             result = multiply(a, b);
             break;
-        case '/' :
+        case '/':
             result = divide(a, b);
             break;
-        case '%' :
+        case '%':
             result = modulus(a, b);
             break;
         default:
@@ -51,5 +43,63 @@ function operate(a, b, operator)
     return result;
 }
 
+let firstNumber;
+let secondNumber;
+let operator;
+let inputData;
+
 const display = document.querySelector(".result-section");
-display.textContent = "Hello";
+
+function resetData() {
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    inputData = '';
+    display.innerText = '';
+}
+
+function showInputOnDisplay(input) {
+    display.textContent = input;
+}
+
+function getNumberFromDisplay() {
+    return +display.textContent;
+}
+
+const buttonClickEventHandler = function (e) {
+
+    const clickedItem = e.target.innerText;
+
+    if (clickedItem == 'AC') {
+        resetData();
+    }
+    else if (+clickedItem >= 0 && +clickedItem <= 9) {
+        inputData += clickedItem;
+        showInputOnDisplay(inputData);
+    }
+    else if (clickedItem == '=') {
+        
+        if(operator === null) {
+            return;
+        }
+        secondNumber = getNumberFromDisplay();
+        let result = operate(firstNumber, secondNumber, operator);
+        operator = null;
+        showInputOnDisplay(result);
+    }
+    else {
+        if (operator !== null) {
+            secondNumber = getNumberFromDisplay();
+            let result = operate(firstNumber, secondNumber, operator);
+            showInputOnDisplay(result);
+        }
+        operator = clickedItem;
+        firstNumber = getNumberFromDisplay();
+        inputData = '';
+    }
+}
+
+resetData();
+
+const buttons = document.querySelector(".button-section");
+buttons.addEventListener('click', buttonClickEventHandler);
